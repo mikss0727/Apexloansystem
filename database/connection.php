b868a0458ed2e05533c58d15beaa8f49f4547eac
JasonImawesome <jasonmacalindong19@gmail.com>
2023-08-04 16:01:40 +0800

    create branch

--- database/connection.php
+++ database/connection.php
@@ -5,14 +5,17 @@
 	//mysqli_select_db(connection,dbname);
 	
 	$host="localhost";
-	$username="u277381793_apexloan";
-	$password="Mikoangelo27$";
+	// $username="u277381793_apexloan";
+	// $password="Mikoangelo27$";
+	$username="root";
+	$password="";
 	$err1="Error Can't connect to MySQL";
 	$err2="Error Can't connect to Database";
 	
 	
 	$con=mysqli_connect($host,$username,$password) or die ($err1);
-	mysqli_select_db($con,'u277381793_loandb') or die ($err2);
+	// mysqli_select_db($con,'u277381793_loandb') or die ($err2);
+	mysqli_select_db($con,'loandb') or die ($err2);
 	
 	?>
     
