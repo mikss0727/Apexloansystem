b99b536d1cdb10d3752247fba2fefd9b9a132992
JasonImawesome <jasonmacalindong19@gmail.com>
2023-08-06 12:36:42 +0800

    update code structure and branch module

--- database/connection.php
+++ database/connection.php
@@ -5,16 +5,17 @@
 	//mysqli_select_db(connection,dbname);
 	
 	$host="localhost";
-	$username="u277381793_ApexFunding";
-	$password="P@ssword1";
-	// $username="root";
-	// $password="";
+	// $username="u277381793_ApexFunding";
+	// $password="P@ssword1";
+	$username="root";
+	$password="";
 	$err1="Error Can't connect to MySQL";
 	$err2="Error Can't connect to Database";
 	
 	
 	$con=mysqli_connect($host,$username,$password) or die ($err1);
-	mysqli_select_db($con,'u277381793_ApexLoanDb') or die ($err2);
+	// mysqli_select_db($con,'u277381793_ApexLoanDb') or die ($err2);
+	mysqli_select_db($con,'loandb') or die ($err2);
 	
 	?>
     
